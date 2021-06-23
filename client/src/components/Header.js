ad529b896b6218c46b87cb25f944e140eda22437
Thomas McErlean <tjmcerlean@googlemail.com>
2021-06-23 17:35:16 +0100

    amend header component and login screen

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@
 const Header = () => {
     return (
         <div className="flex border-b justify-between border-gray-500 h-4 p-4 py-8">
-            <div className="flex items-center">
+            <div className="flex items-center text-4xl">
                 Blog.
             </div>
         </div>
--- client/src/screens/Login.js
+++ client/src/screens/Login.js
@@ -7,26 +7,56 @@ const Login = () => {
 
     const history = useHistory();
 
-    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
     
-    const isEmailValid = (email.includes("@") && email.includes("."));
+    const isUsernameValid = (username !== '' && username.length > 2);
     const isPasswordValid = (password !== '' && password.length > 5);
-    const isLoginValid = isEmailValid && isPasswordValid;
+    const isLoginValid = isUsernameValid && isPasswordValid;
 
     useEffect(() => {
         document.title = 'Login';
     }, []);
 
+    const signIn = (e) => {
+
+        e.preventDefault();
+
+        // Do something
+    }
+
     return (
-        <div>
+        <div className="bg-gray-100">
             <Header />
-            <div className="flex h-screen bg-gray-100">
-                <div className="container m-auto w-1/4 align-middle bg-gray-300">
-                    <p>Blog</p>
+            <div className="container h-screen mx-auto flex flex-col flex-wrap">
+                <div className="container border rounded m-auto w-1/4 flex flex-wrap justify-center bg-white shadow-new">
+                    <h1 className="text-3xl m-4">Blog.</h1>
+                    <form className="flex flex-wrap justify-center">
+                        <input 
+                            className="border rounded w-9/12 p-1 mb-2 pl-2 bg-gray-200 outline-none" 
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <input 
+                            className="border rounded w-9/12 p-1 mb-4 pl-2 bg-gray-200 outline-none" 
+                            placeholder="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button 
+                            className={`border rounded w-9/12 p-1 mb-4 bg-blue-400 text-white font-medium ${isLoginValid ? "cursor-pointer" : "bg-opacity-50 cursor-default"}`}
+                            onClick={signIn}
+                        >Log In</button>
+                        <div className="mb-4">
+                            <p>Don't have an account? <Link to={ROUTES.SIGNUP} className="font-medium text-blue-500 cursor-pointer">Sign up</Link></p>
+                        </div>
+                        {error.length > 0 && <p className="text-xs text-red-600">{error}</p>}
+                    </form>
                 </div>
-            </div>
+            </div>    
         </div>
     )
 
