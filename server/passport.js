683a2e3ac62c8ca324a57511528dc3c0903a34fc
Thomas McErlean <tjmcerlean@googlemail.com>
2021-06-23 23:45:20 +0100

    add passport-jwt strategy to passport.js

--- server/passport.js
+++ server/passport.js
@@ -1,5 +1,8 @@
 const localStrategy = require("passport-local").Strategy;
 const User = require("../models/user");
+const passportJWT = require("passport-jwt");
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 // Passport auth
 passport.use(
@@ -32,3 +35,19 @@ passport.use(
 );
 
 
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey : 'your_jwt_secret'
+},
+function (jwtPayload, cb) {
+
+    //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
+    return UserModel.findOneById(jwtPayload.id)
+        .then(user => {
+            return cb(null, user);
+        })
+        .catch(err => {
+            return cb(err);
+        });
+}
+));
