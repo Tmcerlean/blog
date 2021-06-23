262f4cc3bfb3666b0a35f857847291579e927318
Thomas McErlean <tjmcerlean@googlemail.com>
2021-06-23 23:40:39 +0100

    amend login post controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,9 +3,9 @@ const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
 
-exports.login_post = function(req, res, next) {
+exports.login_post = async (req, res, next) => {
     // return res.json('Received a POST HTTP method');
-    passport.authenticate('local', {session: false}, (err, user, info) => {
+    passport.authenticate('local', {session: false}, async (err, user, info) => {
         if (err || !user) {
             return res.status(400).json({
                 message: 'Something is not right',
@@ -13,17 +13,18 @@ exports.login_post = function(req, res, next) {
             });
         }
 
-    req.login(user, {session: false}, (err) => {
+        req.login(user, {session: false}, async (err) => {
             if (err) {
                 res.send(err);
             }
 
-    // generate a signed son web token with the contents of user object and return it in the response
+        // generate a signed son web token with the contents of user object and return it in the response
 
-    const token = jwt.sign(user, 'your_jwt_secret');
+        const token = jwt.sign(user, 'your_jwt_secret');
             return res.json({user, token});
-            });
-        })(req, res);
+        });
+
+    })(req, res);
 };
 
 exports.logout_post = function(req, res, next) {
