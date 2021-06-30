83d434c13413336a62712c4194adaf95c79d72b1
Thomas McErlean <tjmcerlean@googlemail.com>
2021-06-30 23:23:53 +0100

    add virtual for formatted date to Post Schema

--- client/src/screens/Admin.js
+++ client/src/screens/Admin.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 const Admin = () => {
 
     useEffect(() => {
-        document.title = 'Blog Homepage';
+        document.title = 'Admin Page';
     }, []);
 
     return (
--- server/models/post.js
+++ server/models/post.js
@@ -5,12 +5,22 @@ var Schema = mongoose.Schema;
 var PostSchema = new Schema(
   {
     title: {type: String, required: true, minLength: 1, maxLength: 300},
-    body: {type: String, required: true, minLength: 20},
+    body: {type: String, required: true, minLength: 1},
     published: {type: Boolean, required: true},
     timestamp: {type: Date, default: Date.now, required: true},
     user: {type: Schema.Types.ObjectId, ref: 'User', required: true}
   }
 );
+
+PostSchema.virtual("date_formated").get(function () {
+  return this.date.toLocaleDateString("en-gb", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minutes: "2-digit",
+  });
+});
 
 //Export model
 module.exports = mongoose.model('Post', PostSchema);
