bea1026abc16075d78430f6072c6827969d186b2
Thomas McErlean <tjmcerlean@googlemail.com>
2021-07-03 23:46:40 +0100

    fix bug preventing comments from being stored in db

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -1,4 +1,5 @@
 const { body, validationResult } = require('express-validator');
+const Comment = require("../models/comment");
 
 exports.get_comment = function(req, res, next) {
     return res.json('Received a GET HTTP method');
@@ -8,9 +9,33 @@ exports.get_comments = function(req, res, next) {
     return res.json('Received a GET HTTP method');
 }
 
-exports.create_comment = function(req, res, next) {
-    return res.json('Received a POST HTTP method');
-}
+exports.create_comment = [
+    body("user").trim().isLength({ min: 1 }).withMessage("Empty user field").escape(),
+    body("body").trim().isLength({ min: 1 }).withMessage("Empty comment field").escape(),
+  
+    async function (req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.json({
+                data: req.body,
+                errors: errors.array(),
+            });
+        return;
+        }
+
+        const { body, user } = req.body;
+        const postId = req.params.postid;
+        const comment = new Comment({ body, user, postId });
+        comment.save((err) => {
+            if (err) {
+                return next(err);
+            }
+            res.status(200).json({ 
+                message: "Comment posted" 
+            });
+        });
+    },
+];
 
 exports.edit_comment = function(req, res, next) {
     return res.json('Received a PUT HTTP method');
